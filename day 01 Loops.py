5c820446bae19a9d3072351bbd6e6e14a1319898
Zeeshan Haider <z.haider1504@gmail.com>
2026-04-22 12:19:00 +0500

    Fix logic and formatting in Loops.py
    Fixed indentation and added functionality to find largest and smallest numbers in a list. Also, improved character count logic and corrected Floyd's triangle printing.

--- day 01 Loops.py
+++ day 01 Loops.py
@@ -65,3 +65,46 @@ for i in range(rows):
 for i in range(rows):
     print(" " *(i),"*" * (cols), sep="")
     cols -= 2
+
+#Largest and smallest in a list using loop
+numbers=[3,4,6,4,3,6,7,8654,3,3,5]
+largest=numbers[0]
+smallest=numbers[0]
+for i in range(len(numbers)):
+    if numbers[i]>largest:
+        largest=numbers[i]
+    elif numbers[i]<smallest:
+        smallest=numbers[i]
+print("Largest Number: ",largest)
+print("Smallest Number: ",smallest)
+
+
+# #Finding the count of each character in a word 
+word=input("enter the word to find count of each character")
+visited=[]
+for alphabet in word:
+    word_count=0
+    if alphabet in visited:
+        continue;
+    else:
+        visited.append(alphabet)
+        for char in word:
+            if char==alphabet:
+                word_count+=1
+        print("count of the ",alphabet," in ",word," you provided is ",word_count)
+
+# ---printing the floyd triangle-------------
+# 1
+# 23
+# 456
+# 78910
+# 1112131415
+rows=5
+counter=1
+for row in range(rows):
+    col=0
+    while col<=row:
+        print(counter,end="")
+        counter+=1
+        col+=1
+    print()
