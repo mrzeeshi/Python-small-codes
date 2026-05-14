6112ecd684f09d4f4462e2132eb4db3f8915bd13
Zeeshan Haider <z.haider1504@gmail.com>
2026-05-14 23:08:25 +0500

    changes made to pandas file

--- pandas/Day 09 Pandas.py
+++ pandas/Day 09 Pandas.py
@@ -127,7 +127,6 @@ print(df.groupby("Calories")["Duration"].describe())
 
 #so it groups all the rows that have the same values of calories and then it returns the description of the Duration columns for those similar values of the calories
 #we can use it as below that will return the description of the groups of the rows that are similar on the bases of the columns having the similar values which we are combining by using the group by function
-<<<<<<< HEAD
 print(df.groupby(["Duration", "Calories"]).describe())
 
 print("Here is the mean of the values \n",df.mean())
@@ -136,6 +135,12 @@ print("Here is the mode of the values \n",df.mode())
 #mode gives us the most repeated values in our dataset
 print("Here is written the median of the values \n",df.median())
 #median gives us the values of center row if there are odd numbers of rows and mean of two center rows if there are the even numbers of rows
-=======
-print(df.groupby(["Duration", "Calories"]).describe())
->>>>>>> 0d6fb63e79db30f5bf3ed839c8ecf89db988fb0a
+
+
+#today we will try to use some functions of map rank and some other functions on the pandas dataframes
+#we have the dataframe named as df and we want to map the values that if  the value of pulse is greater than 100 then make a new column that says that it has the max pulse value yes or not
+
+df['ismaxpulse']=df["Pulse"].map(
+    lambda x:"Yes" if x>100 else "No"
+)
+print("\n",df.head)
